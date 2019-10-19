33af89bc37ebd8c618576ffd3a4beee63e2e8ed3
Chris Ritchie <nullbort@gmail.com>
2019-10-19 10:51:50 -0400

    Add code

--- SpaceShip.ino
+++ SpaceShip.ino
@@ -1,27 +1,27 @@
+/*
+ * SpaceShip.ino
+ * 
+ * Copyright 2019 Chris <nullbort@gmail.com>
+ * 
+ * 19.10.2019
+ * 
+ * 
+ */
+
+
 #include <jled.h>
 
-// Candle on LED pin 13 (PWM capable). 
-auto led = JLed(13).Candle();
+// Define LED pins
+const byte outerLEDs = 1;
+const byte innerLEDs = 0;
+
+JLed leds[2] = {
+  JLed(outerLEDs).Candle(5,100),
+  JLed(innerLEDs).Candle(5,100)};
 
 void setup() { }
 
 void loop() {
-  led.Update();
-}
-
-/*
- *
- *JLed leds[5] = {
-    JLed(3).Breathe(2000).Forever(),
-    JLed(4).Blink(750, 250).Forever(),
-    JLed(5).FadeOff(1000).Forever(),
-    JLed(6).FadeOn(1000).Forever(),
-    JLed(LED_BUILTIN).Blink(500, 500).Forever()};
-
-void setup() {
-}
-
-void loop() {
     for (auto& led : leds) {led.Update();}
     delay(1);
-}/*
+}
